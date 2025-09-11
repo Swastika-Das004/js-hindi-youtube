2d83520d79216bcfa44fd140a08c3bcec8e50f59
Swastika-Das004 <dasswastika275@gmail.com>
2025-09-12 00:34:47 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict" /* treat sll JS code as new version of JS */
+
+// alert(3+3)  we are using node.js not browser thatswhy we can;t write this
+
+let name="swastika"
+let age= 20
+let isLoggedIn= false
+
+
+//theses all are premitive datatype--> so here are different types of data types...
+
+//number (range= 2 to the power 53)
+//bigint
+//string (which we can write within "" or without)
+//boolean (true/false)
+//null (itv is a standalone value)(null = given but intentionally empty.)
+//undefined (Meaning: A variable is declared but not assigned any value.)
+//Symbol (unique)
+
+
+//object
+
+console.log(typeof "swastika")
+//it will ofc print the datatype that is string
+
+console.log(typeof age)
+//it will print the datatype as number
+
+console.log(typeof null)
+// it will define the datatype as OBJECT(exeptional)(so we have to remember it that NULL is object )
+
+console.log(typeof undefined)
+//but this undefined will show the datatype as undefined(but these undefined is not an object)
