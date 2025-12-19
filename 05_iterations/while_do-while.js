30c5fd600d28bc7b50b7d52f15dcf849c6ca58a6
Swastika-Das004 <dasswastika275@gmail.com>
2025-12-20 00:36:22 +0530

    do_while

---
+++ 05_iterations/while_do-while.js
@@ -0,0 +1,30 @@
+let index = 0
+
+while ( index<= 10) {
+    //console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let  billus = ["ginu", 'manchu', 'pinchu']
+
+let arr = 0
+while (arr < billus.length) {
+    //console.log(`value is ${billus[arr]}`);
+    arr = arr+1;
+}
+
+//do- while loop
+
+// let score = 1
+do {
+    console.log(`score is ${score}`); 
+    score++
+}while (score <=10) ;
+
+
+let score = 11
+do {
+    console.log(`score is ${score}`); 
+    score++
+}while (score <=10) ;
+
