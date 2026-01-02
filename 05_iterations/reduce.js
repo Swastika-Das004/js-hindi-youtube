e230e49ccc546cdaab5bf9976b5646283bff3c67
Swastika-Das004 <dasswastika275@gmail.com>
2026-01-03 00:31:09 +0530

    reduce

--- 05_iterations/reduce.js
+++ 05_iterations/reduce.js
@@ -1,9 +1,34 @@
 const myNums = [1, 2, 3]
 
-const myTotal = myNums.reduce( function( acc, currval){
-    console.log( `acc: ${acc} and current_val: ${currval}`);
+// const myTotal = myNums.reduce( function( acc, currval){
+//     console.log( `acc: ${acc} and current_val: ${currval}`);
     
-    return acc+currval
-} , 0) // whatever value we put here after coma it is the initial value of accumulator after that it gradually increases
+//     return acc+currval
+// } , 0) // whatever value we put here after coma it is the initial value of accumulator after that it gradually increases
 
+// console.log(myTotal);
+
+// how can we use reduce in arrow function->
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 console.log(myTotal);
+
+const shopping =[
+    {
+       itemName: "JS",
+       pric: 2999 
+    },
+    {
+       itemName: "py",
+       pric: 999 
+    },
+    {
+       itemName: "c",
+       pric: 29199 
+    } 
+]
+
+// if we want to add all the prices->
+const priceToPay= shopping.reduce( (acc,item) => acc + item.pric, 0)
+//console.log(priceToPay);
+
